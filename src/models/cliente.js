d77d24e74f39f29d483a9dee34ef46a53cf7fcf9
Nacelio M. Barbosam <naceliombdev@gmail.com>
2023-07-01 18:18:38 -0300

    criada primeira rota

---
+++ src/models/cliente.js
@@ -0,0 +1,27 @@
+class Cliente {
+  constructor(
+    id,
+    name,
+    mail,
+    cpf,
+    dateBirth,
+    address,
+    phone,
+    regDate,
+    attDate,
+    acvite
+  ) {
+    this.id = id;
+    this.name = name;
+    this.mail = mail;
+    this.cpf = cpf;
+    this.dateBirth = dateBirth;
+    this.address = address;
+    this.phone = phone;
+    this.regDate = regDate;
+    this.attDate = attDate;
+    this.acvite = acvite;
+  }
+}
+
+module.exports = Cliente;
---
+++ src/models/middleware/validators.js
@@ -0,0 +1,39 @@
+const clientValidation = (dataBody) => {
+  const {
+    id,
+    name,
+    mail,
+    cpf,
+    dateBirth,
+    address,
+    phone,
+    regDate,
+    attDate,
+    acvite,
+  } = dataBody;
+
+  // Verificar se todos os campos obrigatórios estão presentes
+  if (
+    !id ||
+    !name ||
+    !mail ||
+    !cpf ||
+    !dateBirth ||
+    !address ||
+    !phone ||
+    !regDate ||
+    !attDate ||
+    acvite === undefined
+  ) {
+    return false;
+  }
+
+  // Outras validações específicas para cada campo
+  // ...
+
+  // Retornar true se todos os campos passarem pelas validações
+  return true;
+};
+
+
+module.exports = clientValidation;
