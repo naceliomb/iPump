92575990c96782bdabecaecd9489fd873caa6d26
Nacelio M. Barbosam <naceliombdev@gmail.com>
2023-07-23 20:38:58 -0300

    adicionado demais rotas de clientes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const Cliente = require("./src/models/cliente");
 const clientValidation = require("./src/middleware/validators");
-const {generateUniqueId} = require("./src/middleware/helpers");
+const { generateUniqueId } = require("./src/middleware/helpers");
 const app = express();
 const port = process.env.PORT || 3000;
 const bd = require("./db/conn");
@@ -24,7 +24,7 @@ app.post("/createUser", (req, res) => {
     const data = req.body; //pegando dados da requisição
     const valid = clientValidation(data); //validando os dados
     if (!valid.status) return res.status(500).send({ "message:": "Dados inválidos!" }); //se os dados forem inválidos, retorna uma mensagem de erro
-    const cliente = { ...valid.data, id: generateUniqueId()}
+    const cliente = { ...valid.data, id: generateUniqueId() };
     console.log(cliente);
     Cliente.create(cliente)
         .then((clienteCriado) => {
@@ -34,9 +34,48 @@ app.post("/createUser", (req, res) => {
             console.error("Erro ao criar cliente:", erro);
             return res.status(500).send({ "message:": "Erro ao criar cliente!" });
         });
-
 });
 
+app.put("/updateUser/:id", (req, res) => {
+    const data = req.body;
+    const valid = clientValidation(data);
+    if (!valid.status) return res.status(500).send({ "message:": "Dados inválidos!" });
+    const id = req.params.id;
+    Cliente.update(valid.data, { where: { id } })
+        .then((clienteAtualizado) => {
+            if (clienteAtualizado[0] === 0) return res.status(404).send({ "message:": "Cliente não encontrado!" });
+            return res.status(200).send({ "message:": "Cliente atualizado com sucesso!" });
+        })
+        .catch((erro) => {
+            console.error("Erro ao atualizar cliente:", erro);
+            return res.status(500).send({ "message:": "Erro ao atualizar cliente!" });
+        });
+});
+
+app.get("/listUsers", (req, res) => {
+    const length = parseInt(req.query.length) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const offset = (page - 1) * length;
+    Cliente.findAndCountAll({ limit: length, offset })
+        .then((clientes) => {
+            return res.status(200).send(clientes);
+        });
+});
+
+app.get("/user/:id", (req, res) => {
+    const id = req.params.id;
+    Cliente.findByPk(id)
+        .then((cliente) => {
+            if (!cliente) return res.status(404).send({ "message:": "Cliente não encontrado!" });
+            return res.status(200).send(cliente);
+        })
+        .catch((erro) => {
+            console.error("Erro ao buscar cliente:", erro);
+            return res.status(500).send({ "message:": "Erro ao buscar cliente!" });
+        });
+});
+
+   
 app.listen(port, () => {
     console.log(`app running at http://localhost:${port}`);
 });
--- tests/cliente.test.js
+++ tests/cliente.test.js
@@ -1,39 +1,59 @@
-const { DataTypes } = require('sequelize');
-const Cliente = require('../src/models/cliente');
-const sequelize = require('../db/conn');
+const { DataTypes } = require("sequelize");
+const Cliente = require("../src/models/cliente");
+const sequelize = require("../db/conn");
 
 // Teste para verificar se o modelo do cliente foi definido corretamente
-describe('Teste do modelo do cliente', () => {
-  beforeAll(async () => {
-    // Conectar ao banco de dados de teste
-    await sequelize.authenticate();
-    console.log('Conexão estabelecida com sucesso.');
-  });
-
-  afterAll(async () => {
-    // Fechar a conexão com o banco de dados de teste
-    await sequelize.close();
-    console.log('Conexão encerrada com sucesso.');
-  });
-
-  test('Deve criar uma instância válida do modelo do cliente', () => {
-    // Cria uma instância do modelo do cliente
-    const cliente = Cliente.build({
-      name: 'John Doe',
-      mail: 'john@example.com',
-      cpf: '12345678900',
-      dateBirth: '1990-01-01',
-      address: '123 Main St',
-      phone: '1234567890',
+describe("Teste do modelo do cliente", () => {
+    beforeAll(async () => {
+        // Conectar ao banco de dados de teste
+        await sequelize.authenticate();
+        console.log("Conexão estabelecida com sucesso.");
     });
 
-    // Verifica se a instância foi criada corretamente
-    expect(cliente instanceof Cliente).toBeTruthy();
-    expect(cliente.name).toBe('John Doe');
-    expect(cliente.mail).toBe('john@example.com');
-    expect(cliente.cpf).toBe('12345678900');
-    expect(cliente.dateBirth.toISOString().split('T')[0]).toBe('1990-01-01');
-    expect(cliente.address).toBe('123 Main St');
-    expect(cliente.phone).toBe('1234567890');
-  });
+    afterAll(async () => {
+        // Fechar a conexão com o banco de dados de teste
+        await sequelize.close();
+        console.log("Conexão encerrada com sucesso.");
+    });
+
+    test("Deve criar uma instância válida do modelo do cliente", () => {
+        // Cria uma instância do modelo do cliente
+        const cliente = Cliente.build({
+            name: "John Doe",
+            mail: "john@example.com",
+            cpf: "12345678900",
+            dateBirth: "1990-01-01",
+            address: "123 Main St",
+            phone: "1234567890",
+        });
+
+        // Verifica se a instância foi criada corretamente
+        expect(cliente instanceof Cliente).toBeTruthy();
+        expect(cliente.name).toBe("John Doe");
+        expect(cliente.mail).toBe("john@example.com");
+        expect(cliente.cpf).toBe("12345678900");
+        expect(cliente.dateBirth.toISOString().split("T")[0]).toBe("1990-01-01");
+        expect(cliente.address).toBe("123 Main St");
+        expect(cliente.phone).toBe("1234567890");
+    });
+
+    test("Deve criar uma instância válida do modelo do cliente com campos opcionais", () => {
+        // Cria uma instância do modelo do cliente
+        const cliente = Cliente.build({
+            name: "John Doe",
+            mail: "john@example.com",
+            cpf: "12345678900",
+            dateBirth: "1990-01-01",
+            address: "123 Main St",
+            phone: "1234567890",
+            complement: "Apt 1",
+            district: "Downtown",
+            city: "Anytown",
+            state: "NY",
+            country: "USA",
+        });
+
+        expect(cliente).toBeInstanceOf(Cliente);
+    });
+    
 });
